0e15a711a4a3eafaa1198a43715ba436fc94789e
Damon <yank0362@qq.com>
2020-07-30 20:32:02 +0800

    前台分期付款详情页

--- routes/web.php
+++ routes/web.php
@@ -48,6 +48,8 @@ Route::group(['middleware' => ['auth', 'verified']], function() {
 
     Route::post('payment/{order}/installment', 'PaymentController@payByInstallment')->name('payment.installment');
     Route::get('installments', 'InstallmentsController@index')->name('installments.index');
+
+    Route::get('installments/{installment}', 'InstallmentsController@show')->name('installments.show');
 });
 
 Route::post('payment/alipay/notify', 'PaymentController@alipayNotify')->name('payment.alipay.notify');
